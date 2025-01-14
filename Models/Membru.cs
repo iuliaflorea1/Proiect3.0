e5dd4d1d10059647634805bd8ab94ab12ba73c1f
iuliaflorea1 <iulia@IULIA_FLOREA>
2025-01-14 13:47:14 +0200

    final?

--- Models/Membru.cs
+++ Models/Membru.cs
@@ -14,9 +14,10 @@ namespace Proiect3._0.Models
         public string? NumeMembru { get; set; }
         [StringLength(70)]
         public string? Adresa { get; set; }
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Adresa de email nu este validă.")]
 
         public string Email { get; set; }
-        [RegularExpression(@"^\(?([0-9]{4})\)?[-. ]?([0-9]{3})[-. ]?([09]{3})$", ErrorMessage = "Telefonul trebuie sa fie de forma '0722-123-123' sau '0722.123.123' sau '0722 123 123'")] 
+        [RegularExpression(@"^\(?([0-9]{4})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})$", ErrorMessage = "Telefonul trebuie sa fie de forma '0722-123-123', '0722.123.123' sau '0722 123 123'")]
         public string? Telefon { get; set; }
 
         public ICollection<Participare>? Participari { get; set; }
